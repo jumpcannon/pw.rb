f729532ef8f556d17a0977ec9f05efa2c2ed1e8a
Morgan <jump@fastmail.fm>
2014-05-26 14:23:43 -0700

    BYTE ARE PRECIOUS SAVE THEM

--- pw.rb
+++ pw.rb
@@ -3,8 +3,4 @@
 # Creates a password, per https://xkcd.com/936/
 # usage: pw.rb
 
-puts File.readlines('/usr/share/dict/words')
-  .map { |line| line.rstrip }
-  .select { |line| (line =~ /^[a-z]+$/) && (2 < line.length) && (line.length < 8)}
-  .sample(4)
-  .join(" ")
+puts File.readlines('/usr/share/dict/words').map { |line| line.rstrip }.select { |line| (line =~ /^[a-z]+$/) && (2 < line.length) && (line.length < 8)}.sample(4).join(" ")
